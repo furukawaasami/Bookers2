a9d9f5baf7824447467706a4affb077341deb605
EC2 Default User <ec2-user@ip-172-31-41-197.ec2.internal>
2022-02-21 09:47:44 +0000

    1950

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,13 @@
 class BooksController < ApplicationController
 
-
    def create
-     book = Book.new(book_params)
-     book.user_id = current_user.id
-     if book.save
-       redirect_to book_path(book.id)
+     @book = Book.new(book_params)
+     @book.user_id = current_user.id
+     if @book.save
+       redirect_to book_path(@book.id)
      else
        @books = Book.all
+       @user = current_user
        render :index
      end
    end
@@ -40,10 +40,11 @@ class BooksController < ApplicationController
      book.destroy
      redirect_to books_path
    end
+
+
  private
-  # ストロングパラメータ
+
   def book_params
     params.require(:book).permit(:title, :body, :user_id)
   end
-
 end
