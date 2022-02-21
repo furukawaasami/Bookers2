67ecc2efdc0411e5671d62a1a9ae8f0ff4a34f17
EC2 Default User <ec2-user@ip-172-31-41-197.ec2.internal>
2022-02-21 16:26:32 +0000

    template book shoe ok

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,8 +21,7 @@ class BooksController < ApplicationController
    def show
      @book = Book.find(params[:id])
      @newbook = Book.new
-     @books = Book.all
-     @user = books.user
+     @user = @book.user
    end
 
    def edit
